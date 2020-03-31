434ea88abc732783f5e34825155523b5b15545d9
Nikita Zharkovsky <zharkovsky@angelsit.ru>
2020-03-31 18:01:37 +0300

    Create pleasure section

---
+++ app/src/main/java/com/example/antidepression/PleasureActivity.java
@@ -0,0 +1,14 @@
+package com.example.antidepression;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PleasureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pleasure);
+    }
+}
