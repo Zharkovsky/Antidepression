1c08f7ce1934a239ceb423e12661a5163d52b705
Alex Masyukov <rrrazor@outlook.com>
2020-04-07 09:44:25 +0300

    Added a slider with some advices

--- app/src/main/java/com/example/antidepression/AdviceScreenSlidePagerActivity.java
+++ app/src/main/java/com/example/antidepression/AdviceScreenSlidePagerActivity.java
@@ -2,7 +2,82 @@ package com.example.antidepression;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class AdviceScreenSlidePagerActivity extends FragmentActivity {
+    String[] advices = new String[] {
+            "Заведите личный дневник (описывайте в нем события, сопутствующие мысли и чувства",
+            "Ежедневно составляйте план на день с конкретными и реалистичными целями",
+            "Включите в распорядок дня мероприятия, приносящие положительные эмоции (встречи с друзьями, хобби и т.п.)",
+            "Уделите время физическим нагрузкам",
+            "Найдите момент для творчества, созидания",
+            "Фокусируйтесь на позитивных моментах дня (в абсолютно любой ситуации есть свои плюсы, находите их)",
+            "Каждый вечер пишите 10 благодарностей за прошедший день (людям, Богу, событиям)",
+            "Помните, что для достижения результата нужно время, будте терпеливы",
+    };
+
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 8;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.advice_activity_screen_slide);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // if (mPager.getCurrentItem() == 0) {
+        //     // If the user is currently looking at the first step, allow the system to handle the
+        //     // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        // } else {
+        //     // Otherwise, select the previous step.
+        //     mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        // }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new AdviceScreenSlidePageFragment(advices[position], position);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
 }
